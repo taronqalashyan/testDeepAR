7e2b6abb0646ceb22db883f53dbdb9e06fc657ad
Taron Kalashyan <taronqalashyan@gmail.com>
2024-04-21 22:30:45 +0400

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# quickstart-web-npm-js
+
+This is a quickstart project for DeepAR Web SDK. It is a minimal example of how to use the SDK.
+
+## View the demo
+
+[https://demo.deepar.ai/quickstart-web-js-npm/](https://demo.deepar.ai/quickstart-web-js-npm/)
+
+## How to run the project
+
+- Go to https://developer.deepar.ai
+  - Sign up
+  - Create a project and a Web app
+  - Copy the license key
+  - Paste it into `src/index.js` (replace `your_license_key_goes_here`)
+- Open the terminal in the root of the project
+  - Run `npm install`
+  - Run `npm run dev`
+  - If the browser doesn't open automatically, open http://localhost:8888
+
+## How to build the project
+
+- Open the terminal in the root of the project
+  - Run `npm install`
+  - Run `npm run build`
+  - The build will be in the `dist` folder
---
+++ src/index.js
@@ -0,0 +1,84 @@
+import * as deepar from "deepar";
+import Carousel from "./carousel.js";
+
+// Log the version. Just in case.
+console.log("Deepar version: " + deepar.version);
+
+// Top-level await is not supported.
+// So we wrap the whole code in an async function that is called immediatly.
+(async function () {
+
+  const loadingScreen = document.getElementById("loading-screen");
+  loadingScreen.display = 'none';
+  // Get the element you want to place DeepAR into. DeepAR will inherit its width and height from this and fill it.
+  const previewElement = document.getElementById("ar-screen");
+
+
+
+  // trigger loading progress bar animation
+  const loadingProgressBar = document.getElementById("loading-progress-bar");
+  loadingProgressBar.style.width = "100%";
+
+  // All the effects are in the public/effects folder.
+  // Here we define the order of effect files.
+  const effectList = [
+    "effects/ray-ban-wayfarer.deepar",
+    "effects/viking_helmet.deepar",
+    "effects/MakeupLook.deepar",
+    "effects/Split_View_Look.deepar",
+    "effects/flower_face.deepar",
+    "effects/Stallone.deepar",
+    "effects/galaxy_background_web.deepar",
+    "effects/Humanoid.deepar",
+    "effects/Neon_Devil_Horns.deepar",
+    "effects/Ping_Pong.deepar",
+    "effects/Pixel_Hearts.deepar",
+    "effects/Snail.deepar",
+    "effects/Hope.deepar",
+    "effects/Vendetta_Mask.deepar",
+    "effects/Fire_Effect.deepar",
+  ];
+
+  let deepAR = null;
+
+  // Initialize DeepAR with an effect file.
+  try {
+    deepAR = await deepar.initialize({
+      licenseKey: "e78cbe1cb26bea08da3237392e8856831c5d647c233549e65b488c7d697b5ffbf7175057dc57c7ce``",
+      previewElement,
+      effect: effectList[0],
+      // Removing the rootPath option will make DeepAR load the resources from the JSdelivr CDN,
+      // which is fine for development but is not recommended for production since it's not optimized for performance and can be unstable.
+      // More info here: https://docs.deepar.ai/deepar-sdk/platforms/web/tutorials/download-optimizations/#custom-deployment-of-deepar-web-resources
+      rootPath: "./deepar-resources",
+      additionalOptions: {
+        cameraConfig: {
+          // facingMode: 'environment'  // uncomment this line to use the rear camera
+        },
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("permission-denied-screen").style.display = "block";
+    return;
+  }
+
+  // Hide the loading screen.
+  document.getElementById("loading-screen").style.display = "none";
+  document.getElementById("ar-screen").style.display = "block";
+
+  window.effect = effectList[0];
+
+  const glassesCarousel = new Carousel("carousel");
+  glassesCarousel.onChange = async (value) => {
+    const loadingSpinner = document.getElementById("loading-spinner");
+
+    if (window.effect !== effectList[value]) {
+      loadingSpinner.style.display = "block";
+      await deepAR.switchEffect(effectList[value]);
+      window.effect = effectList[value];
+    }
+    loadingSpinner.style.display = "none";
+  };
+})();
